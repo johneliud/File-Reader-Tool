ca499fa88f04a3cfbe0166470ad2942bce529296
John <johneliud4@gmail.com>
2024-06-13 21:15:57 +0300

    Chore: 1. Adopt bufio.NewScanner() instead of os.ReadFile() to read information from the input file. 2. Check for errors when reading data from an empty input file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -9,30 +10,54 @@ import (
 func main() {
 	// Check for incorrect number of arguments passed
 	if len(os.Args) != 3 {
-		fmt.Println("Error! Incorrect number of arguments passed. Usage: go run . input-file.txt output-file.txt")
+		fmt.Println("Incorrect number of arguments passed. Usage: go run . input.txt output.txt")
 		return
 	}
 
 	arguments := os.Args[1:]
 
+	// Check if the file extensions associated with input and output isn't .txt
 	if filepath.Ext(arguments[0]) != ".txt" || filepath.Ext(arguments[1]) != ".txt" {
-		fmt.Println("Error! Incorrect file extension.")
-    return
-	}
-
-	// inputFile stores the contents of the read file 
-	inputFile, err := os.ReadFile(arguments[0])
-	if err != nil {
-		fmt.Println("Error! Cannot read from file.")
+		fmt.Println("Incorrect file extension used on file.")
 		return
 	}
 
-	// Writes whatever is read from inputFile and specifies file permissions of the new file
-	err = os.WriteFile(arguments[1], []byte(inputFile), 0o644)
+	inputTxt, err := os.Open(arguments[0])
 	if err != nil {
-		fmt.Println("Error! Cannot write to file")
+		fmt.Println("Failed opening input file.", err)
+		return
+	}
+	defer inputTxt.Close()
+
+	// Variable inputTxtDetails contains the file properties of the input file using the os.Stat() method
+	inputTxtDetails, err := os.Stat(arguments[0])
+	if err != nil {
+		fmt.Println("Failed to obtain input file properties.", err)
 		return
 	}
 
-	fmt.Println("File modification successful")
+	inputTxtSize := inputTxtDetails.Size()
+	if inputTxtSize == 0 {
+		fmt.Println("Cannot read from an empty input file.")
+		return
+	}
+
+	outputTxt, err := os.Create(arguments[1])
+	if err != nil {
+		fmt.Println("Failed to create output file.", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(inputTxt)
+	for scanner.Scan() {
+		readLine := scanner.Text()
+
+		_, err := outputTxt.Write([]byte(readLine))
+		if err != nil {
+			fmt.Println("Cannot write to output file.", err)
+			return
+		}
+	}
+
+	fmt.Println("File modification successful.")
 }
